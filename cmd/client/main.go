f257643d7decd9984dfb08e7c31ee49cd2ecd6ff
fischor <21199007+fischor@users.noreply.github.com>
2021-09-01 15:55:32 +0200

    refactor: add interruptcontext package
    Adds the interruptcontext package that handles context cancelation and
    graceful interrupts.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,20 +4,10 @@ import (
 	"context"
 	"github.com/fischor/dew/internal/command"
 	"os"
-	"os/signal"
 )
 
 func main() {
-	// Setup ctx. Notifies the client when to stop.
-	// Is closed when a os.Interrupt is received.
-	ctx, cancel := context.WithCancel(context.Background())
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt)
-	go func() {
-		<-sig
-		cancel()
-	}()
-
+	ctx := context.Background()
 	cmd := command.NewRootCommand(os.Stdin, os.Stdout, os.Stderr)
 	if err := cmd.ExecuteContext(ctx); err != nil {
 		os.Exit(1)

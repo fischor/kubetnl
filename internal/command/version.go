0f244ecde70b4952245a082ead23c06996e0388d
fischor <21199007+fischor@users.noreply.github.com>
2021-09-02 13:44:24 +0200

    feat: add version command
    The version command prints the current release version of kubetnl and
    the git commit the binary was build against.

---
+++ internal/command/version.go
@@ -0,0 +1,52 @@
+package command
+
+import (
+	"fmt"
+
+	"github.com/fischor/kubetnl/internal/version"
+	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/kubectl/pkg/util/templates"
+)
+
+type VersionOptions struct {
+	genericclioptions.IOStreams
+	Short bool
+}
+
+var (
+	versionExample = templates.Examples(`
+                # Print the version number and git commit SHA.
+                kubetnl version
+
+		# Print the version number only and omit the git commit SHA.
+                kubetnl version --short`)
+)
+
+func NewVersionCommand(streams genericclioptions.IOStreams) *cobra.Command {
+	o := &VersionOptions{
+		IOStreams: streams,
+	}
+
+	cmd := &cobra.Command{
+		Use:     "version [--short]",
+		Short:   "Print the kubetnl version",
+		Example: cleanupExamples,
+		Run: func(cmd *cobra.Command, args []string) {
+			o.Run()
+		},
+	}
+
+	cmd.Flags().BoolVar(&o.Short, "short", o.Short, "If true, just prints the version number and omits git commit SHA.")
+
+	return cmd
+}
+
+func (o *VersionOptions) Run() {
+	i := version.Get()
+	if o.Short {
+		fmt.Fprintf(o.Out, "%s\n", i.Version)
+	} else {
+		fmt.Fprintf(o.Out, "%s at %s\n", i.Version, i.GitCommit)
+	}
+}
---
+++ internal/version/version.go
@@ -0,0 +1,46 @@
+// Package version provides version information for kubetnl.
+package version
+
+import "strings"
+
+var (
+	// Release version of kubetnl.
+	version = "development"
+
+	// NOTE: The $Format strings are replaced during 'git archive' thanks
+	// to the companion .gitattributes file containing 'export-subst' in
+	// this same directory.  See also
+	// https://git-scm.com/docs/gitattributes
+	gitCommit = "$Format:%H$" // sha1 from git, output of $(git rev-parse HEAD)
+)
+
+// Info holds version information for kubetnl.
+type Info struct {
+	// Version is the release version of kubetnl. Format follows the rules
+	// of semantic versioning.
+	Version string
+
+	// GitCommit is the git commit SHA the kubetnl binary was build against.
+	GitCommit string
+}
+
+// Get returns the current version information.
+func Get() Info {
+	comm := gitCommit
+	// HACK: Check if the build happens neither via a go build
+	// github.com/fischor/kubetnl or go install github.com/fischor/kubentl
+	// nor with provided ldflags.
+	chars := strings.Split(gitCommit, "")
+	if len(chars) == 11 && chars[0] == "$" && chars[10] == "$" {
+		comm = "unknown commit"
+	}
+	return Info{
+		Version:   version,
+		GitCommit: comm,
+	}
+}
+
+// String returns i.Version.
+func (i Info) String() string {
+	return i.Version
+}
